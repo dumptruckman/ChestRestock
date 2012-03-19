ac18795d0346318bc8873685504bb59b6ee3e2b9
Jeremy Wood <farachan@gmail.com>
2012-03-19 16:52:08 -0400

    Readded accidently deleted working copy of new file.

---
+++ src/main/java/com/dumptruckman/chestrestock/util/Perms.java
@@ -0,0 +1,10 @@
+package com.dumptruckman.chestrestock.util;
+
+import com.dumptruckman.minecraft.pluginbase.permission.Perm;
+
+public class Perms {
+    
+    public static final Perm CMD_SET = new Perm.Builder("cmd.set").build();
+    public static final Perm CMD_CHECK = new Perm.Builder("cmd.check").build();
+    public static final Perm CMD_UPDATE = new Perm.Builder("cmd.update").build();
+}
